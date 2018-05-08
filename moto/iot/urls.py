d4ae26d0db734111330339af3b23f445160ea0a6
Brian Rower <Brian.Rower@gmail.com>
2018-05-08 12:05:36 -0700

    Fix iot urls

--- moto/iot/urls.py
+++ moto/iot/urls.py
@@ -6,9 +6,25 @@ url_bases = [
 ]
 
 
-response = IoTResponse()
-
-
 url_paths = {
-    '{0}/.*$': response.dispatch,
+    '{0}/endpoint$': IoTResponse.dispatch,
+    '{0}/keys-and-certificate$': IoTResponse.dispatch,
+    '{0}/certificates$': IoTResponse.dispatch,
+    '{0}/certificates/<certificateId>$': IoTResponse.dispatch,
+    '{0}/policies$': IoTResponse.dispatch,
+    '{0}/policies/<policyName>$': IoTResponse.dispatch,
+    '{0}/principals/things$': IoTResponse.dispatch,
+    '{0}/principal-policies$': IoTResponse.dispatch,
+    '{0}/principal-policies/<policyName>$': IoTResponse.dispatch,
+    '{0}/things$': IoTResponse.dispatch,
+    '{0}/things/<thingName>$': IoTResponse.dispatch,
+    '{0}/things/<thingName>/principals$': IoTResponse.dispatch,
+    '{0}/things/<thingName>/thing-groups$': IoTResponse.dispatch,
+    '{0}/thing-groups$': IoTResponse.dispatch,
+    '{0}/thing-groups/addThingToThingGroup$': IoTResponse.dispatch,
+    '{0}/thing-groups/removeThingFromThingGroup': IoTResponse.dispatch,
+    '{0}/thing-groups/<thingGroupName>$': IoTResponse.dispatch,
+    '{0}/thing-groups/<thingGroupName>/things$': IoTResponse.dispatch,
+    '{0}/thing-types$': IoTResponse.dispatch,
+    '{0}/thing-types/<thingTypeName>$': IoTResponse.dispatch,
 }
